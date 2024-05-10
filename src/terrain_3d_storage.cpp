eef9ed114f0577083f636dfbd57744d2492a2b3e
Matt <greyworld@gmail.com>
2024-05-10 10:28:24 -0400

    Whitespace fix to remove a conflict

--- src/terrain_3d_storage.cpp
+++ src/terrain_3d_storage.cpp
@@ -675,7 +675,8 @@ void Terrain3DStorage::import_images(const TypedArray<Image> &p_images, Vector3 
 	Vector3 descaled_position = p_global_position / vertex_spacing;
 	int max_dimension = _region_size * REGION_MAP_SIZE / 2;
 	if ((abs(descaled_position.x) > max_dimension) || (abs(descaled_position.z) > max_dimension)) {
-		LOG(ERROR, "Specify a position within +/-", Vector3(max_dimension, 0.f, max_dimension) * vertex_spacing);		return;
+		LOG(ERROR, "Specify a position within +/-", Vector3(max_dimension, 0.f, max_dimension) * vertex_spacing);
+		return;
 	}
 	if ((descaled_position.x + img_size.x > max_dimension) ||
 			(descaled_position.z + img_size.y > max_dimension)) {
